9cf3ebbdc3dacf817ba9d03cf6d4a6f2a30816bc
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-07-20 18:19:58 +0100

    Split the void pipeline message classes

--- tests/Pragmatic.CQRS.Benchmark/Handlers/VoidMessage.cs
+++ tests/Pragmatic.CQRS.Benchmark/Handlers/VoidMessage.cs
@@ -17,27 +17,3 @@ public class VoidMessageHandler : IRequestHandler<VoidMessage>
         return Task.CompletedTask;
     }
 }
-
-public class VoidPipelineMessage : IRequest
-{
-    public VoidPipelineMessage(int count)
-        => Count = count;
-
-    public int Count { get; set; }
-}
-
-public class VoidPipelineMessageHandler : IRequestHandler<VoidPipelineMessage>
-{
-    public Task Handle(VoidPipelineMessage request, CancellationToken cancellationToken = default)
-    {
-        return Task.FromResult(request.Count);
-    }
-}
-
-public class VoidPipelineBehaviourHandler : IPipelineBehavior<VoidPipelineMessage>
-{
-    public Task Handle(VoidPipelineMessage input, RequestHandlerDelegate next, CancellationToken cancellationToken = default)
-    {
-        return next();
-    }
-}
---
+++ tests/Pragmatic.CQRS.Benchmark/Handlers/VoidPipelineMessage.cs
@@ -0,0 +1,27 @@
+﻿using Pragmatic.CQRS;
+
+namespace Pragmatic.CQRS.Benchmark.Handlers;
+
+public class VoidPipelineMessage : IRequest
+{
+    public VoidPipelineMessage(int count)
+        => Count = count;
+
+    public int Count { get; set; }
+}
+
+public class VoidPipelineMessageHandler : IRequestHandler<VoidPipelineMessage>
+{
+    public Task Handle(VoidPipelineMessage request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(request.Count);
+    }
+}
+
+public class VoidPipelineBehaviourHandler : IPipelineBehavior<VoidPipelineMessage>
+{
+    public Task Handle(VoidPipelineMessage input, RequestHandlerDelegate next, CancellationToken cancellationToken = default)
+    {
+        return next();
+    }
+}
